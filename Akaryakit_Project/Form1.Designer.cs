d126452b0f0c66e73d082ed8b770f99275fc6084
f4t1hsalih <f4t1hsalih@gmail.com>
2023-08-22 14:10:17 +0300

    İlk tasarım değişiklikleri yapıldı

--- Akaryakit_Project/Form1.Designer.cs
+++ Akaryakit_Project/Form1.Designer.cs
@@ -28,12 +28,452 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            groupBox1 = new GroupBox();
+            label7 = new Label();
+            label8 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label9 = new Label();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            label10 = new Label();
+            button1 = new Button();
+            groupBox3 = new GroupBox();
+            progressBar1 = new ProgressBar();
+            progressBar2 = new ProgressBar();
+            progressBar3 = new ProgressBar();
+            progressBar4 = new ProgressBar();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            textBox9 = new TextBox();
+            label15 = new Label();
+            groupBox4 = new GroupBox();
+            label16 = new Label();
+            label17 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(13, 13);
+            groupBox1.Margin = new Padding(4);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(4);
+            groupBox1.Size = new Size(200, 270);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Price";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(130, 159);
+            label7.Name = "label7";
+            label7.Size = new Size(31, 21);
+            label7.TabIndex = 8;
+            label7.Text = "***";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(27, 159);
+            label8.Name = "label8";
+            label8.Size = new Size(97, 21);
+            label8.TabIndex = 7;
+            label8.Text = "Gaz Otogaz: ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(130, 125);
+            label5.Name = "label5";
+            label5.Size = new Size(31, 21);
+            label5.TabIndex = 6;
+            label5.Text = "***";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(37, 125);
+            label6.Name = "label6";
+            label6.Size = new Size(87, 21);
+            label6.TabIndex = 5;
+            label6.Text = "Pro Diesel: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(130, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 21);
+            label3.TabIndex = 4;
+            label3.Text = "***";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(32, 90);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 21);
+            label4.TabIndex = 3;
+            label4.Text = "Max Diesel: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(130, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 21);
+            label2.TabIndex = 2;
+            label2.Text = "***";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(17, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Kurşunsuz95: ";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label15);
+            groupBox2.Controls.Add(textBox9);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Controls.Add(textBox5);
+            groupBox2.Controls.Add(textBox6);
+            groupBox2.Controls.Add(textBox7);
+            groupBox2.Controls.Add(textBox8);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(textBox3);
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Location = new Point(221, 13);
+            groupBox2.Margin = new Padding(4);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(4);
+            groupBox2.Size = new Size(221, 270);
+            groupBox2.TabIndex = 9;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Selling";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(7, 52);
+            textBox1.MaxLength = 4;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(96, 29);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(7, 87);
+            textBox2.MaxLength = 4;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(96, 29);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(7, 122);
+            textBox3.MaxLength = 4;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(96, 29);
+            textBox3.TabIndex = 2;
+            textBox3.Text = "0";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(7, 156);
+            textBox4.MaxLength = 4;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(96, 29);
+            textBox4.TabIndex = 3;
+            textBox4.Text = "0";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(7, 23);
+            label9.Name = "label9";
+            label9.Size = new Size(41, 21);
+            label9.TabIndex = 4;
+            label9.Text = "Liter";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(118, 156);
+            textBox5.MaxLength = 10;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(96, 29);
+            textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(118, 122);
+            textBox6.MaxLength = 10;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(96, 29);
+            textBox6.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(118, 87);
+            textBox7.MaxLength = 10;
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(96, 29);
+            textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(118, 52);
+            textBox8.MaxLength = 10;
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(96, 29);
+            textBox8.TabIndex = 5;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(118, 23);
+            label10.Name = "label10";
+            label10.Size = new Size(44, 21);
+            label10.TabIndex = 9;
+            label10.Text = "Price";
+            // 
+            // button1
+            // 
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(54, 229);
+            button1.Name = "button1";
+            button1.Size = new Size(118, 34);
+            button1.TabIndex = 10;
+            button1.Text = "Depo Doldur";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(label14);
+            groupBox3.Controls.Add(label13);
+            groupBox3.Controls.Add(label12);
+            groupBox3.Controls.Add(label11);
+            groupBox3.Controls.Add(progressBar4);
+            groupBox3.Controls.Add(progressBar3);
+            groupBox3.Controls.Add(progressBar2);
+            groupBox3.Controls.Add(progressBar1);
+            groupBox3.Location = new Point(450, 13);
+            groupBox3.Margin = new Padding(4);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Padding = new Padding(4);
+            groupBox3.Size = new Size(257, 203);
+            groupBox3.TabIndex = 11;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Fullness";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(7, 52);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(199, 29);
+            progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            progressBar2.Location = new Point(7, 87);
+            progressBar2.Name = "progressBar2";
+            progressBar2.Size = new Size(199, 29);
+            progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            progressBar3.Location = new Point(7, 122);
+            progressBar3.Name = "progressBar3";
+            progressBar3.Size = new Size(199, 29);
+            progressBar3.TabIndex = 2;
+            // 
+            // progressBar4
+            // 
+            progressBar4.Location = new Point(7, 157);
+            progressBar4.Name = "progressBar4";
+            progressBar4.Size = new Size(199, 29);
+            progressBar4.TabIndex = 3;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(212, 55);
+            label11.Name = "label11";
+            label11.Size = new Size(40, 21);
+            label11.TabIndex = 5;
+            label11.Text = "0,00";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(212, 90);
+            label12.Name = "label12";
+            label12.Size = new Size(40, 21);
+            label12.TabIndex = 6;
+            label12.Text = "0,00";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(212, 125);
+            label13.Name = "label13";
+            label13.Size = new Size(40, 21);
+            label13.TabIndex = 7;
+            label13.Text = "0,00";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(212, 159);
+            label14.Name = "label14";
+            label14.Size = new Size(40, 21);
+            label14.TabIndex = 8;
+            label14.Text = "0,00";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(118, 191);
+            textBox9.MaxLength = 11;
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(96, 29);
+            textBox9.TabIndex = 11;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(7, 194);
+            label15.Name = "label15";
+            label15.Size = new Size(102, 21);
+            label15.TabIndex = 12;
+            label15.Text = "License Plate:";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(label17);
+            groupBox4.Controls.Add(label16);
+            groupBox4.Location = new Point(450, 218);
+            groupBox4.Margin = new Padding(4);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Padding = new Padding(4);
+            groupBox4.Size = new Size(257, 65);
+            groupBox4.TabIndex = 12;
+            groupBox4.TabStop = false;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.ForeColor = Color.Black;
+            label16.Location = new Point(12, 27);
+            label16.Name = "label16";
+            label16.Size = new Size(46, 21);
+            label16.TabIndex = 14;
+            label16.Text = "Case:";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.ForeColor = Color.Black;
+            label17.Location = new Point(64, 27);
+            label17.Name = "label17";
+            label17.Size = new Size(40, 21);
+            label17.TabIndex = 15;
+            label17.Text = "0,00";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(58, 108, 107);
+            ClientSize = new Size(720, 296);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = Color.FromArgb(231, 229, 191);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Akaryakıt";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private Label label7;
+        private Label label8;
+        private Label label5;
+        private Label label6;
+        private Label label3;
+        private Label label4;
+        private Label label2;
+        private GroupBox groupBox2;
+        private Label label10;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Label label9;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button1;
+        private GroupBox groupBox3;
+        private Label label14;
+        private Label label13;
+        private Label label12;
+        private Label label11;
+        private ProgressBar progressBar4;
+        private ProgressBar progressBar3;
+        private ProgressBar progressBar2;
+        private ProgressBar progressBar1;
+        private Label label15;
+        private TextBox textBox9;
+        private GroupBox groupBox4;
+        private Label label16;
+        private Label label17;
     }
 }
